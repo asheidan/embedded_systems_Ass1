25cf6fd4fa79134169b9bdf1794a429bf5557bb8
Emil Eriksson <asheidan@gmail.com>
2010-04-25 23:08:10 +0200

    Moved the delay function.

--- src/leds.c
+++ src/leds.c
@@ -1,15 +1,18 @@
 #include <avr/io.h>
+#include "delay.h"
 #include "leds.h"
-
-void delay() {
-	unsigned char a,b;
-	
-	for(a = 0; a < 5; a++) {
-		for(b = 0; b < 255; b++);
-	}
-}
 
 void LED_On(int i) {
 	PORTD = _BV(i);
-	// delay();
+	delay();
+}
+
+int main() {
+	DDRD = 0xFF;
+	unsigned char i;
+	
+	for(i = 0;;i++) {
+		PORTD = i;
+		delay();
+	}
 }
